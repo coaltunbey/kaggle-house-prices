14d516efec65fdcb7b2ca4f04e5770e46ecbd7fe
Cem Ozan Altunbey <coaltunbey@gmail.com>
2018-02-19 21:10:54 +0300

    eda initialization

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -9,8 +10,142 @@ from scipy import stats
 from scipy.stats import norm
 from sklearn.preprocessing import StandardScaler
 
-df_train = pd.read_csv('../data/train.csv')
+# Set main path
+main_path = os.getcwd() + '/AnacondaProjects/kaggle-house-prices'
+
+# Read data tables
+df_train = pd.read_csv(main_path + '/data/train.csv')
+df_test = pd.read_csv(main_path + '/data/test.csv')
+
+# Take a first glance at the data
+df_train.columns
+
+# SalePrice column's descriptive statistics summary
+df_train['SalePrice'].describe()
+
+# SalePrice distribution histogram
+sns.distplot(df_train['SalePrice']);
 
 
-import os
-cwd = os.getcwd()
+## Scatter plot grlivarea/saleprice
+
+# Bind SalePrice and GrLivArea columns together
+data = pd.concat([df_train['SalePrice'], df_train['GrLivArea']], axis=1)
+
+# SalePrice increases with GrLivArea, "ylim" ==  Y-axis maximum
+data.plot.scatter(x='GrLivArea', y='SalePrice', ylim=(0,800000));
+
+## Scatter plot totalbsmtsf/saleprice
+data = pd.concat([df_train['SalePrice'], df_train['TotalBsmtSF']], axis=1)
+data.plot.scatter(x='TotalBsmtSF', y='SalePrice', ylim=(0,800000));
+
+
+# Box plot overallqual/saleprice
+data = pd.concat([df_train['SalePrice'], df_train['OverallQual']], axis=1)
+f, ax = plt.subplots(figsize=(8, 6))
+fig = sns.boxplot(x='OverallQual', y="SalePrice", data=data)
+fig.axis(ymin=0, ymax=800000);
+
+# Box plot yearbuilt/saleprice
+data = pd.concat([df_train['SalePrice'], df_train['YearBuilt']], axis=1)
+f, ax = plt.subplots(figsize=(12, 6))
+fig = sns.boxplot(x='YearBuilt', y="SalePrice", data=data)
+fig.axis(ymin=0, ymax=800000);
+plt.xticks(rotation=90);
+
+## !! YearBuilt/SalePrice inflation effect ? 
+
+## Correlation matrix
+corrmat = df_train.corr()
+f, ax = plt.subplots(figsize=(12, 9))
+sns.heatmap(corrmat, vmax=.8, square=True);
+
+## !! multicollinearity between garage variables ? 
+## !! garageCars/garageArea -- TotalBasementArea / 1stFloorArea
+
+
+## Saleprice correlation matrix
+k = 10 #number of variables for heatmap
+cols = corrmat.nlargest(k, 'SalePrice')['SalePrice'].index
+cm = np.corrcoef(df_train[cols].values.T)
+sns.set(font_scale=1.25)
+hm = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size': 10}, yticklabels=cols.values, xticklabels=cols.values)
+plt.show()
+
+## Seaborn scatter scatterplots
+sns.set()
+cols = ['SalePrice', 'OverallQual', 'GrLivArea', 'GarageCars', 'TotalBsmtSF', 'FullBath', 'YearBuilt']
+sns.pairplot(df_train[cols], size = 1.5)
+plt.show();
+
+## Missing data
+total = df_train.isnull().sum().sort_values(ascending=False)
+percent = (df_train.isnull().sum()/df_train.isnull().count()).sort_values(ascending=False)
+missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+missing_data.head(20)
+
+## Dealing with missing data
+## Delete columns with NA, since they are not important except Electrical, delete it's observation
+df_train = df_train.drop((missing_data[missing_data['Total'] > 1]).index,1)
+df_train = df_train.drop(df_train.loc[df_train['Electrical'].isnull()].index)
+df_train.isnull().sum().max() ## check if there is any null value
+
+## Standardizing data
+saleprice_scaled = StandardScaler().fit_transform(df_train['SalePrice'][:,np.newaxis]);
+low_range = saleprice_scaled[saleprice_scaled[:,0].argsort()][:10]
+high_range= saleprice_scaled[saleprice_scaled[:,0].argsort()][-10:]
+print('outer range (low) of the distribution:')
+print(low_range)
+print('\nouter range (high) of the distribution:')
+print(high_range)
+
+## SalePrice normality test
+
+# Histogram and normal probability plot
+sns.distplot(df_train['SalePrice'], fit=norm);
+fig = plt.figure()
+res = stats.probplot(df_train['SalePrice'], plot=plt)
+
+## !! SalePrice looks not normal
+
+# Applying log transformation
+df_train['SalePrice'] = np.log(df_train['SalePrice'])
+
+# Transformed histogram and normal probability plot
+sns.distplot(df_train['SalePrice'], fit=norm);
+fig = plt.figure()
+res = stats.probplot(df_train['SalePrice'], plot=plt)
+
+## GrLivArea normality test
+
+# Histogram and normal probability plot
+sns.distplot(df_train['GrLivArea'], fit=norm);
+fig = plt.figure()
+res = stats.probplot(df_train['GrLivArea'], plot=plt)
+
+## !! Needs to be transformed
+df_train['GrLivArea'] = np.log(df_train['GrLivArea'])
+
+# Transformed histogram and normal probability plot
+sns.distplot(df_train['GrLivArea'], fit=norm);
+fig = plt.figure()
+res = stats.probplot(df_train['GrLivArea'], plot=plt)
+
+## TotalBsmtSF normality test
+
+# Histogram and normal probability plot
+sns.distplot(df_train['TotalBsmtSF'], fit=norm);
+fig = plt.figure()
+res = stats.probplot(df_train['TotalBsmtSF'], plot=plt)
+
+## !! A significant number of observations with value zero (houses without basement).
+## Therefore, use log(x+1)
+
+# Transform data
+df_train['TotalBsmtSF'] = np.log1p(df_train['TotalBsmtSF'])
+
+sns.distplot(df_train[df_train['TotalBsmtSF']>0]['TotalBsmtSF'], fit=norm);
+fig = plt.figure()
+res = stats.probplot(df_train[df_train['TotalBsmtSF']>0]['TotalBsmtSF'], plot=plt)
+
+
